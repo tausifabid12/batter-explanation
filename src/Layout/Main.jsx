2943e1db6cc4908e49b0676653e865fb1db74ec0
tausifabid12 <tasifabid12@gmail.com>
2022-10-27 19:00:23 +0600

    dark theme toggle btn addedd

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -7,6 +7,20 @@ const Main = () => {
   return (
     <div>
       <Header></Header>
+      <div className="fixed bottom-0 right-0 ">
+        <label
+          for="Toggle4"
+          className="inline-flex items-center p-[1.5px] rounded-md cursor-pointer  text-gray-800 border-1"
+        >
+          <input id="Toggle4" type="checkbox" className="hidden peer" />
+          <span className="px-3 py-1  bg-[#aa076b] peer-checked:bg-gray-500 text-white rounded-l-md">
+            Light
+          </span>
+          <span className="px-3 py-1 text-white bg-gray-400  peer-checked:bg-gray-900 rounded-r-md">
+            Dark
+          </span>
+        </label>
+      </div>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
